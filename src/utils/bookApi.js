a50440345241c85d4818ab62bb82afbafb95a652
Suchith357 <saisuchithm69@gmail.com>
2026-04-06 15:01:54 +0530

    Delete src directory

--- src/utils/bookApi.js
+++
@@ -1,9 +0,0 @@
-export const searchBooks = async (query) => {
-  const res = await fetch(
-    `https://www.googleapis.com/books/v1/volumes?q=${query}`
-  );
-
-  const data = await res.json();
-
-  return data.items || [];
-};
--- src/utils/countryApi.js
+++
@@ -1,9 +0,0 @@
-export const searchCountries = async (name) => {
-  const res = await fetch(
-    `https://restcountries.com/v3.1/name/${name}`
-  );
-
-  const data = await res.json();
-
-  return data;
-};
