86a1d9a5f388c687790b36338d3113fc46986fea
Sai Suchith <saisuchithm69@gmail.com>
2026-05-17 20:13:39 +0530

    Changes made to weather app script

--- weather-app/script.js
+++ weather-app/script.js
@@ -1,12 +1,28 @@
-async function getWeather(){
-    let city = document.getElementById("city").value;
-    let apiKey = "0b372a6e66a2c909756510c78c4a8538";
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+let cityInput = document.getElementById("city");
+let result = document.getElementById("result");
+let searchBtn = document.getElementById("searchBtn");
 
-    let response = await fetch(url);
-    let data = await response.json();
-    console.log(data);
-    document.getElementById("result").innerHTML = `
+searchBtn.addEventListener("click",getWeather);
+
+cityInput.addEventListener("keydown",function(event){
+    if(event.key === "Enter") {
+        getWeather();
+    }
+});
+async function getWeather(){
+
+    try {
+        let city = cityInput.value;
+        let apiKey = "0b372a6e66a2c909756510c78c4a8538";
+        result.innerHTML = "<h3>Loading....<h3/>";
+        let url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+        let response = await fetch(url);
+        let data = await response.json();
+        if(data.cod != 200) {
+            result.innerHTML = "<h3>City Not Found</h3>";
+            return;
+        }
+        result.innerHTML = `
 
         <h2>${data.name}</h2>
 
@@ -17,4 +33,9 @@ async function getWeather(){
         <p>Weather: ${data.weather[0].description}</p>
 
     `;
+    }
+    catch(error){
+        result.innerHTML = "<h3>Error Fetching Data</h3>";
+        console.log(error);
+    }  
 }
